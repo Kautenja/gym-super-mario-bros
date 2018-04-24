661201b89cf941b6fd376c8581e152454a7fee5f
Kautenja <ckfour@icloud.com>
2018-04-24 12:57:45 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,17 @@ from setuptools import setup, find_packages
 from nesgym_super_mario_bros import __version__, __url__
 
 
+# open the README to fill the long description key
+with open('README.md') as README:
+    long_description = README.read()
+
+
 setup(
     name='nesgym_super_mario_bros',
     version=__version__,
     description='Super Mario Bros. for Open.ai Gym',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     keywords=['Open.ai Gym', 'NES', 'Super Mario Bros.'],
     url=__url__,
     download_url='{}/archive/{}.tar.gz'.format(__url__, __version__),
