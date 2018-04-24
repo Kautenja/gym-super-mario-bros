d9a9d8ef690a37c55fedd8812bbbd8d7a634e839
Kautenja <ckfour@icloud.com>
2018-04-24 13:12:12 -0500

    rename

---
+++ play_random.py
@@ -0,0 +1,13 @@
+"""A simple script for debugging the Super Mario Bros. Lua code."""
+import gym
+import nesgym_super_mario_bros
+
+
+env = gym.make('nesgym/SuperMarioBros-v0')
+
+
+done = True
+for step in range(5000):
+    if done:
+        state = env.reset()
+    state, reward, done, info = env.step(env.action_space.sample())
