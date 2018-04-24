b8d22ee03f8a545ed9099b13e7497f47a2563066
Kautenja <ckfour@icloud.com>
2018-04-24 16:57:25 -0500

    better play script

--- play_random.py
+++ play_random.py
@@ -1,17 +1,22 @@
 """A simple script for debugging the Super Mario Bros. Lua code."""
 import gym
+from tqdm import tqdm
 import nesgym_super_mario_bros
 
 
-env = gym.make('nesgym/SuperMarioBros-v0')
+def play_game(env):
+    """Play the game."""
+    done = True
+    for step in tqdm(range(5000)):
+        if done:
+            state = env.reset()
+        state, reward, done, info = env.step(env.action_space.sample())
+
+    env.close()
 
 
-done = True
-for step in range(5000):
-    print(step)
-    if done:
-        print('done')
-        state = env.reset()
-    state, reward, done, info = env.step(env.action_space.sample())
-
-env.close()
+try:
+    env = gym.make('nesgym/SuperMarioBros-v0')
+    play_game(env)
+except KeyboardInterrupt:
+    env.close()
