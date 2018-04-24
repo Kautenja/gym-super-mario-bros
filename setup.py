dc2fda0ebd0343c6ca62c5a77381e3f7b2609797
Kautenja <ckfour@icloud.com>
2018-04-24 13:37:54 -0500

    update setup.py to use version from git

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
-from nesgym_super_mario_bros import __version__, __url__
+
+
+# the URL to find the source code and releases
+__url__ = 'https://github.com/Kautenja/nesgym-super-mario-bros'
 
 
 # open the README to fill the long description key
@@ -9,15 +12,15 @@ with open('README.md') as README:
 
 setup(
     name='nesgym_super_mario_bros',
-    # version=__version__,
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
+    setup_requires=[
+        'setuptools-git-version'
+    ],
+    version_format='{tag}',
     description='Super Mario Bros. for Open.ai Gym',
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    # long_description_content_type='text/markdown',
     keywords=['Open.ai Gym', 'NES', 'Super Mario Bros.'],
     url=__url__,
-    # download_url='{}/archive/{}.tar.gz'.format(__url__, __version__),
     author='Christian Kauten',
     author_email='kautencreations@gmail.com',
     license='MIT License',
