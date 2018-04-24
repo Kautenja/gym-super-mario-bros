0839c95fa35ae5c1fa8ebf4726f1de1eb7f6c855
Kautenja <ckfour@icloud.com>
2018-04-24 17:02:07 -0500

    cleanup

--- play_random.py
+++ play_random.py
@@ -1,13 +1,14 @@
 """A simple script for debugging the Super Mario Bros. Lua code."""
 import gym
 from tqdm import tqdm
+# the package needs imported to register the environment with OpenAI Gym
 import nesgym_super_mario_bros
 
 
-def play_game(env):
+def play_game(env, episodes: int=5000):
     """Play the game."""
     done = True
-    for step in tqdm(range(5000)):
+    for step in tqdm(range(episodes)):
         if done:
             state = env.reset()
         state, reward, done, info = env.step(env.action_space.sample())
