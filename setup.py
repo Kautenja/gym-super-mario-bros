271f17eadf4955e7cd6a6972953b6eda82cfb699
Kautenja <ckfour@icloud.com>
2018-04-24 14:03:48 -0500

    attempt to fix Markdown PyPi issue again

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup, find_packages
 
 
-# the URL to find the source code and releases
-__url__ = 'https://github.com/Kautenja/nesgym-super-mario-bros'
-
-
 # open the README to fill the long description key
-with open('README.md') as README:
-    long_description = README.read()
+def README():
+    with open('README.md') as README_file:
+        return README_file.read()
 
 
 setup(
@@ -17,14 +14,16 @@ setup(
     ],
     version_format='{tag}',
     description='Super Mario Bros. for Open.ai Gym',
-    long_description=long_description,
-    long_description_content_type='text/markdown; charset=UTF-8; variant=GFM',
-    keywords=['Open.ai Gym', 'NES', 'Super Mario Bros.'],
-    url=__url__,
+    long_description=README(),
+    long_description_content_type='text/markdown',
+    keywords=['OpenAI-Gym', 'NES', 'Super-Mario-Bros'],
+    url='https://github.com/Kautenja/nesgym-super-mario-bros',
     author='Christian Kauten',
     author_email='kautencreations@gmail.com',
-    license='MIT License',
-    packages=['nesgym_super_mario_bros'],
+    license='MIT',
+    packages=[
+        'nesgym_super_mario_bros'
+    ],
     zip_safe=False,
     install_requires=[
         'gym>=0.10.5',
