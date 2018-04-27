fa989dbb1613e52f96b78c72492b50d39eadd9b1
Kautenja <ckfour@icloud.com>
2018-04-27 00:09:25 -0500

    document

--- gym_super_mario_bros/smb_env.py
+++ gym_super_mario_bros/smb_env.py
@@ -3,34 +3,35 @@ from gym import spaces
 from .nesenv import NESEnv
 
 
-package_directory = os.path.dirname(os.path.abspath(__file__))
-
-
 class SuperMarioBrosEnv(NESEnv):
     """An environment for playing Super Mario Bros with OpenAI Gym."""
 
     def __init__(self, **kwargs) -> None:
-        """
-        Initialize a new Super Mario Bros environment.
-
-        Args:
-            kwargs: the key word arguments to pass to the superclass constructor
-
-        Returns:
-            None
-
-        """
         super().__init__(**kwargs)
-        # setup the paths to the Lua server file and NES ROM.
-        self.lua_interface_path = os.path.join(package_directory, 'lua/super-mario.lua')
-        self.rom_file_path = os.path.join(package_directory, 'roms/super-mario.nes')
+        # load the package directory of this class
+        package_directory = os.path.dirname(os.path.abspath(__file__))
+        # setup the path to the Lua script
+        lua_name = 'lua/super-mario-bros.lua'
+        self.lua_interface_path = os.path.join(package_directory, lua_name)
+        # setup the path to the game ROM
+        rom_name = 'roms/super-mario-bros.nes'
+        self.rom_file_path = os.path.join(package_directory, rom_name)
         # setup the discrete action space for the agent
         self.actions = [
-            '',
-            'U', 'D', 'L', 'R',
-            'LA', 'LB', 'LAB',
-            'RA', 'RB', 'RAB',
-            'A', 'B', 'AB'
+            '',    # NOP
+            'U',   # Up
+            'D',   # Down
+            'L',   # Left
+            'R',   # Right
+            'LA',  # Left + A
+            'LB',  # Left + B
+            'LAB', # Left + A + B
+            'RA',  # Right + A
+            'RB',  # Right + B
+            'RAB', # Right + A + B
+            'A',   # A
+            'B',   # B
+            'AB'   # A + B
         ]
         self.action_space = spaces.Discrete(len(self.actions))
 
