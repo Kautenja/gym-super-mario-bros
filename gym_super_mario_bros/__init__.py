e654a14706ae2597eed7973040a23587d471d5a4
Kautenja <ckfour@icloud.com>
2018-06-03 02:20:55 -0500

    cleanup

--- gym_super_mario_bros/__init__.py
+++ gym_super_mario_bros/__init__.py
@@ -13,6 +13,7 @@ gym.envs.registration.register(
     kwargs={
         'max_episode_steps': math.inf,
         'frame_skip': 4,
+        'lost_levels': False,
         'downsampled_rom': False,
     },
     nondeterministic=True,
@@ -28,10 +29,45 @@ gym.envs.registration.register(
     kwargs={
         'max_episode_steps': math.inf,
         'frame_skip': 4,
+        'lost_levels': False,
         'downsampled_rom': True,
     },
     nondeterministic=True,
 )
+
+
+# Super Mario Bros with 1 frame skip and no down-sampling
+gym.envs.registration.register(
+    id='SuperMarioBrosNoFrameskip-v0',
+    entry_point='gym_super_mario_bros:SuperMarioBrosEnv',
+    max_episode_steps=9999999,
+    reward_threshold=32000,
+    kwargs={
+        'max_episode_steps': math.inf,
+        'frame_skip': 1,
+        'lost_levels': False,
+        'downsampled_rom': False,
+    },
+    nondeterministic=True,
+)
+
+
+# Super Mario Bros with 1 frame skip and down-sampling
+gym.envs.registration.register(
+    id='SuperMarioBrosNoFrameskip-v1',
+    entry_point='gym_super_mario_bros:SuperMarioBrosEnv',
+    max_episode_steps=9999999,
+    reward_threshold=32000,
+    kwargs={
+        'max_episode_steps': math.inf,
+        'frame_skip': 1,
+        'lost_levels': False,
+        'downsampled_rom': True,
+    },
+    nondeterministic=True,
+)
+
+
 
 
 # Super Mario Bros 2 with 4 frame skip and no down-sampling
@@ -60,38 +96,6 @@ gym.envs.registration.register(
         'max_episode_steps': math.inf,
         'frame_skip': 4,
         'lost_levels': True,
-        'downsampled_rom': True,
-    },
-    nondeterministic=True,
-)
-
-
-
-
-# Super Mario Bros with 1 frame skip and no down-sampling
-gym.envs.registration.register(
-    id='SuperMarioBrosNoFrameskip-v0',
-    entry_point='gym_super_mario_bros:SuperMarioBrosEnv',
-    max_episode_steps=9999999,
-    reward_threshold=32000,
-    kwargs={
-        'max_episode_steps': math.inf,
-        'frame_skip': 1,
-        'downsampled_rom': False,
-    },
-    nondeterministic=True,
-)
-
-
-# Super Mario Bros with 1 frame skip and down-sampling
-gym.envs.registration.register(
-    id='SuperMarioBrosNoFrameskip-v1',
-    entry_point='gym_super_mario_bros:SuperMarioBrosEnv',
-    max_episode_steps=9999999,
-    reward_threshold=32000,
-    kwargs={
-        'max_episode_steps': math.inf,
-        'frame_skip': 1,
         'downsampled_rom': True,
     },
     nondeterministic=True,
