a6e4b02a91aa6ce0739a6018357815825cd859ea
Kautenja <ckfour@icloud.com>
2018-06-03 02:10:14 -0500

    register lost levels

--- gym_super_mario_bros/__init__.py
+++ gym_super_mario_bros/__init__.py
@@ -4,6 +4,7 @@ import gym
 from .smb_env import SuperMarioBrosEnv
 
 
+# Super Mario Bros with 4 frame skip and no down-sampling
 gym.envs.registration.register(
     id='SuperMarioBros-v0',
     entry_point='gym_super_mario_bros:SuperMarioBrosEnv',
@@ -17,6 +18,8 @@ gym.envs.registration.register(
     nondeterministic=True,
 )
 
+
+# Super Mario Bros with 4 frame skip and down-sampling
 gym.envs.registration.register(
     id='SuperMarioBros-v1',
     entry_point='gym_super_mario_bros:SuperMarioBrosEnv',
@@ -31,7 +34,41 @@ gym.envs.registration.register(
 )
 
 
+# Super Mario Bros 2 with 4 frame skip and no down-sampling
+gym.envs.registration.register(
+    id='SuperMarioBros2-v0',
+    entry_point='gym_super_mario_bros:SuperMarioBrosEnv',
+    max_episode_steps=9999999,
+    reward_threshold=32000,
+    kwargs={
+        'max_episode_steps': math.inf,
+        'frame_skip': 4,
+        'lost_levels': True,
+        'downsampled_rom': False,
+    },
+    nondeterministic=True,
+)
 
+
+# Super Mario Bros 2 with 4 frame skip and down-sampling
+# gym.envs.registration.register(
+#     id='SuperMarioBros2-v1',
+#     entry_point='gym_super_mario_bros:SuperMarioBrosEnv',
+#     max_episode_steps=9999999,
+#     reward_threshold=32000,
+#     kwargs={
+#         'max_episode_steps': math.inf,
+#         'frame_skip': 4,
+#         'lost_levels': True,
+#         'downsampled_rom': True,
+#     },
+#     nondeterministic=True,
+# )
+
+
+
+
+# Super Mario Bros with 1 frame skip and no down-sampling
 gym.envs.registration.register(
     id='SuperMarioBrosNoFrameskip-v0',
     entry_point='gym_super_mario_bros:SuperMarioBrosEnv',
@@ -45,6 +82,8 @@ gym.envs.registration.register(
     nondeterministic=True,
 )
 
+
+# Super Mario Bros with 1 frame skip and down-sampling
 gym.envs.registration.register(
     id='SuperMarioBrosNoFrameskip-v1',
     entry_point='gym_super_mario_bros:SuperMarioBrosEnv',
@@ -58,6 +97,37 @@ gym.envs.registration.register(
     nondeterministic=True,
 )
 
+
+# Super Mario Bros 2 with 1 frame skip and no down-sampling
+gym.envs.registration.register(
+    id='SuperMarioBros2NoFrameskip-v0',
+    entry_point='gym_super_mario_bros:SuperMarioBrosEnv',
+    max_episode_steps=9999999,
+    reward_threshold=32000,
+    kwargs={
+        'max_episode_steps': math.inf,
+        'frame_skip': 1,
+        'lost_levels': True,
+        'downsampled_rom': False,
+    },
+    nondeterministic=True,
+)
+
+
+# Super Mario Bros 2 with 1 frame skip and down-sampling
+# gym.envs.registration.register(
+#     id='SuperMarioBros2NoFrameskip-v1',
+#     entry_point='gym_super_mario_bros:SuperMarioBrosEnv',
+#     max_episode_steps=9999999,
+#     reward_threshold=32000,
+#     kwargs={
+#         'max_episode_steps': math.inf,
+#         'frame_skip': 1,
+#         'lost_levels': True,
+#         'downsampled_rom': True,
+#     },
+#     nondeterministic=True,
+# )
 
 
 def make(environment: str) -> gym.Env:
