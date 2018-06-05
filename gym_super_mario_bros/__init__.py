c446855d8166cedc110fdc35f0157708ed83c77f
Kautenja <ckfour@icloud.com>
2018-06-04 22:11:49 -0500

    reorganize wrap method

--- gym_super_mario_bros/__init__.py
+++ gym_super_mario_bros/__init__.py
@@ -2,14 +2,7 @@
 import math
 import gym
 from .smb_env import SuperMarioBrosEnv
-from .wrappers import (
-    ClipRewardEnv,
-    DownsampleEnv,
-    FrameStackEnv,
-    MaxFrameskipEnv,
-    PenalizeDeathEnv,
-    RewardCacheEnv,
-)
+from .wrappers import wrap
 
 
 def _register_mario_env(id: str, **kwargs: dict) -> None:
@@ -61,48 +54,6 @@ _register_mario_env('SuperMarioBros2NoFrameskip-v1', lost_levels=True, frame_ski
 def make(environment: str) -> gym.Env:
     """Make the environment and return it. same as `gym.make`."""
     return gym.make(environment)
-
-
-def wrap(env: gym.Env,
-    image_size: tuple=(84, 84),
-    skip_frames: int=4,
-    death_penalty: int=-1,
-    clip_rewards: bool=True,
-    agent_history_length: int=4
-) -> gym.Env:
-    """
-    Wrap an environment with standard wrappers.
-
-    Args:
-        env: the environment to wrap
-        image_size: the size to down-sample images to
-        skip_frames: the number of frames to hold each action for
-        death_penatly: the penalty for losing a life in a game
-        clip_rewards: whether to clip rewards in {-1, 0, +1}
-        agent_history_length: the size of the frame buffer for the agent
-
-    Returns:
-        a gym environment configured for this experiment
-
-    """
-    # wrap the environment with a reward cacher
-    env = RewardCacheEnv(env)
-    # apply the frame skip feature if enabled
-    if skip_frames is not None:
-        env = MaxFrameskipEnv(env, skip=skip_frames)
-    # apply a down-sampler for the given game
-    env = DownsampleEnv(env, image_size)
-    # apply the death penalty feature if enabled
-    if death_penalty is not None:
-        env = PenalizeDeathEnv(env, penalty=death_penalty)
-    # clip the rewards in {-1, 0, +1} if the feature is enabled
-    if clip_rewards:
-        env = ClipRewardEnv(env)
-    # apply the back history of frames if the feature is enabled
-    if agent_history_length is not None:
-        env = FrameStackEnv(env, agent_history_length)
-
-    return env
 
 
 # define the outward facing API of this module (none, gym provides the API)
--- gym_super_mario_bros/wrappers/__init__.py
+++ gym_super_mario_bros/wrappers/__init__.py
@@ -1,4 +1,5 @@
 """Wrappers for altering the functionality of the game."""
+import gym
 from .clip_reward_env import ClipRewardEnv
 from .downsample_env import DownsampleEnv
 from .frame_stack_env import FrameStackEnv
@@ -6,6 +7,48 @@ from .max_frameskip_env import MaxFrameskipEnv
 from .monitor import Monitor
 from .penalize_death_env import PenalizeDeathEnv
 from .reward_cache_env import RewardCacheEnv
+
+
+def wrap(env: gym.Env,
+    image_size: tuple=(84, 84),
+    skip_frames: int=4,
+    death_penalty: int=-1,
+    clip_rewards: bool=True,
+    agent_history_length: int=4
+) -> gym.Env:
+    """
+    Wrap an environment with standard wrappers.
+
+    Args:
+        env: the environment to wrap
+        image_size: the size to down-sample images to
+        skip_frames: the number of frames to hold each action for
+        death_penatly: the penalty for losing a life in a game
+        clip_rewards: whether to clip rewards in {-1, 0, +1}
+        agent_history_length: the size of the frame buffer for the agent
+
+    Returns:
+        a gym environment configured for this experiment
+
+    """
+    # wrap the environment with a reward cacher
+    env = RewardCacheEnv(env)
+    # apply the frame skip feature if enabled
+    if skip_frames is not None:
+        env = MaxFrameskipEnv(env, skip=skip_frames)
+    # apply a down-sampler for the given game
+    env = DownsampleEnv(env, image_size)
+    # apply the death penalty feature if enabled
+    if death_penalty is not None:
+        env = PenalizeDeathEnv(env, penalty=death_penalty)
+    # clip the rewards in {-1, 0, +1} if the feature is enabled
+    if clip_rewards:
+        env = ClipRewardEnv(env)
+    # apply the back history of frames if the feature is enabled
+    if agent_history_length is not None:
+        env = FrameStackEnv(env, agent_history_length)
+
+    return env
 
 
 # explicitly define the outward facing API of this package
@@ -17,4 +60,5 @@ __all__ = [
     Monitor.__name__,
     PenalizeDeathEnv.__name__,
     RewardCacheEnv.__name__,
+    wrap.__name__,
 ]
