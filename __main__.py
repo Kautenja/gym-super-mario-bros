4847d93279faae4426ae30953b824336751dc963
Kautenja <ckfour@icloud.com>
2018-06-05 00:37:59 -0500

    CLI

---
+++ __main__.py
@@ -0,0 +1,6 @@
+"""The main execution script for this package for testing."""
+from gym_super_mario_bros.cli import main
+
+
+# execute the main entry point of the CLI
+main()
--- gym_super_mario_bros/cli.py
+++ gym_super_mario_bros/cli.py
@@ -1,5 +1,48 @@
-"""The main entry point when executing the package as a program."""
+"""Super Mario Bros for OpenAI Gym."""
+import argparse
+from ._registration import make
+from .wrappers import wrap
 
 
-def main():
-    print('main')
+def create_argparser() -> argparse.ArgumentParser:
+    """Create and return an argument parser for this command line interface."""
+    parser = argparse.ArgumentParser(description=__doc__)
+    # add the argument for the Super Mario Bros environment to run
+    parser.add_argument('--env', '-e',
+        type=str,
+        default='SuperMarioBros-v0',
+        choices=[
+            'SuperMarioBros-v0',
+            'SuperMarioBros-v1',
+            'SuperMarioBros-v2',
+            'SuperMarioBros-v3',
+            'SuperMarioBrosNoFrameskip-v0',
+            'SuperMarioBrosNoFrameskip-v1',
+            'SuperMarioBrosNoFrameskip-v2',
+            'SuperMarioBrosNoFrameskip-v3',
+            'SuperMarioBros2-v0',
+            'SuperMarioBros2-v1',
+            'SuperMarioBros2NoFrameskip-v0',
+            'SuperMarioBros2NoFrameskip-v1',
+        ],
+        help='The name of the environment to play.'
+    )
+    # add the argument for the mode of execution as either human or random
+    parser.add_argument('--mode', '-m',
+        type=str,
+        default='human',
+        choices=['human', 'random'],
+        help='The execution mode as either `human` or `random`.'
+    )
+
+    return parser
+
+
+def main() -> None:
+    """The main entry point for the command line interface."""
+    args = create_argparser().parse_args()
+    env_name = args.env
+    mode = args.mode
+
+    print(env_name)
+    print(mode)
