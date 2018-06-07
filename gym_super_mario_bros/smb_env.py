03b7bd669785ce47359a74259a513b5093d1f804
Kautenja <ckfour@icloud.com>
2018-06-07 00:46:23 -0500

    integrate world and level selection

--- gym_super_mario_bros/smb_env.py
+++ gym_super_mario_bros/smb_env.py
@@ -19,7 +19,7 @@ class SuperMarioBrosEnv(NESEnv):
         Args:
             rom_mode: the ROM mode to use when loading ROMs from disk. valid
                 options are:
-                - 'standard': the standard ROM with no modifications
+                -  None: the standard ROM with no modifications
                 - 'downsample': down-sampled ROM with static artifacts removed
                 - 'pixel': a simpler pixelated version of graphics
                 - 'rectangle': an even simpler rectangular version of graphics
@@ -45,7 +45,7 @@ class SuperMarioBrosEnv(NESEnv):
             elif rom_mode == 'downsample':
                 rom_name = 'roms/super-mario-bros-2-downsampled.nes'
             else:
-                raise ValueError('invalid rom_mode ({})'.format(rom_mode))
+                raise ValueError('invalid rom_mode: {}'.format(repr(rom_mode)))
         else:
             if rom_mode is None:
                 rom_name = 'roms/super-mario-bros.nes'
@@ -56,7 +56,7 @@ class SuperMarioBrosEnv(NESEnv):
             elif rom_mode == 'downsample':
                 rom_name = 'roms/super-mario-bros-downsampled.nes'
             else:
-                raise ValueError('invalid rom_mode ({})'.format(rom_mode))
+                raise ValueError('invalid rom_mode: {}'.format(repr(rom_mode)))
         # convert the path to an absolute path
         self.rom_file_path = os.path.join(package_directory, rom_name)
         # setup the discrete action space for the agent
