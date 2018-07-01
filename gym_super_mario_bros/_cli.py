29c1de300d8392b799a184ad01f2951d3802a68b
Kautenja <kautencreations@gmail.com>
2018-07-01 13:24:38 -0500

    update for human play

--- gym_super_mario_bros/_cli.py
+++ gym_super_mario_bros/_cli.py
@@ -1,9 +1,8 @@
 """Super Mario Bros for OpenAI Gym."""
-import sys, argparse
+import os
+import argparse
 from ._registration import make
 from ._play import play_human, play_random
-from .nes_env import headless
-from .wrappers import wrap
 
 
 def create_argparser() -> argparse.ArgumentParser:
@@ -22,28 +21,17 @@ def create_argparser() -> argparse.ArgumentParser:
         choices=['human', 'random'],
         help='The execution mode for the emulation.'
     )
-    # add a flag for running in headless mode
-    parser.add_argument('--headless', '-H',
-        action='store_true',
-        help='A flag to run the emulation without a GUI window.'
-    )
 
     return parser
 
 
 def main() -> None:
     """The main entry point for the command line interface."""
-    # parse arguments from the command line (args are validated by argparse)
+    # parse arguments from the command line (argparse validates arguments)
     args = create_argparser().parse_args()
-    # if the headless flag is specified, disable the GUI window before
-    # building any environments
-    if args.headless:
-        headless()
     # select the method for playing the game
     if args.mode == 'human':
-        if args.headless:
-            print('human mode cannot run with (--headless, -H) enabled')
-            sys.exit(-1)
+        os.environ['HUMAN_PLAY'] = str(True)
         play = play_human
     elif args.mode == 'random':
         play = play_random
