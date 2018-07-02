2cde7154e057e7792abad4eb70165dd6257cf903
Kautenja <ckfour@icloud.com>
2018-07-01 20:39:32 -0500

    cleanup

--- gym_super_mario_bros/wrappers/penalize_death_env.py
+++ gym_super_mario_bros/wrappers/penalize_death_env.py
@@ -1,11 +1,12 @@
 """A gym wrapper for penalizing deaths."""
 import gym
+import numpy as np
 
 
 class PenalizeDeathEnv(gym.Wrapper):
     """a wrapper that penalizes deaths, without terminating episodes."""
 
-    def __init__(self, env, penalty: int=-10) -> None:
+    def __init__(self, env: gym.Env, penalty: int=-15) -> None:
         """
         Initialize a new death penalizing environment wrapper.
 
@@ -17,16 +18,29 @@ class PenalizeDeathEnv(gym.Wrapper):
             None
 
         """
-        gym.Wrapper.__init__(self, env)
+        super().__init__(env)
         self.penalty = penalty
 
-    def step(self, action):
+    def step(self, action: any) -> tuple:
+        """
+        Take a step using the given action.
+        Args:
+            action: the discrete action to perform.
+        Returns:
+            a tuple of:
+            -   the start as a result of the action
+            -   the reward achieved by taking the action
+            -   a flag denoting whether the episode has ended
+            -   a dictionary of extra information
+        """
         obs, reward, done, info = self.env.step(action)
+        # update the reward based on the done flag
         reward = self.penalty if done else reward
 
         return obs, reward, done, info
 
-    def reset(self):
+    def reset(self) -> np.ndarray:
+        """Reset the emulator and return the initial state."""
         return self.env.reset()
 
 
