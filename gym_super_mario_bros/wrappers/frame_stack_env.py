a1cd3a73c0fcbd9941e93c660665c3c6840ab13f
Kautenja <ckfour@icloud.com>
2018-07-01 20:49:01 -0500

    cleanup

--- gym_super_mario_bros/wrappers/frame_stack_env.py
+++ gym_super_mario_bros/wrappers/frame_stack_env.py
@@ -1,22 +1,29 @@
 """An environment wrapper to stack observations into a tensor."""
 from collections import deque
-import numpy as np
 import gym
+import numpy as np
+from ._util import LazyFrames
 
 
 class FrameStackEnv(gym.Wrapper):
     """An environment wrapper to stack observations into a tensor."""
 
-    def __init__(self, env, k):
-        """Stack k last frames.
-        Returns lazy array, which is much more memory efficient.
-        See Also
-        --------
-        baselines.common.atari_wrappers.LazyFrames
+    def __init__(self, env: gym.Env, k: int) -> None:
         """
-        gym.Wrapper.__init__(self, env)
+        Initialize a wrapper to stack the last k frames.
+
+        Args:
+            env: the environment to wrap
+            k: the number of previous frames to stack together
+
+        Returns:
+            None
+
+        """
+        super().__init__(env)
         self.k = k
         self.frames = deque([], maxlen=k)
+        # setup the new observation space based on the k frame skip
         shp = env.observation_space.shape
         self.observation_space = gym.spaces.Box(
             low=0,
@@ -25,53 +32,39 @@ class FrameStackEnv(gym.Wrapper):
             dtype=np.uint8
         )
 
-    def reset(self):
-        ob = self.env.reset()
-        for _ in range(self.k):
-            self.frames.append(ob)
-        return self._get_ob()
-
-    def step(self, action):
-        ob, reward, done, info = self.env.step(action)
-        self.frames.append(ob)
-        return self._get_ob(), reward, done, info
-
-    def _get_ob(self):
+    def _get_ob(self) -> LazyFrames:
+        """Return a LazyFrames object of the stack of frame."""
         assert len(self.frames) == self.k
         return LazyFrames(list(self.frames))
 
+    def reset(self) -> LazyFrames:
+        """Reset the emulator and return the initial state."""
+        ob = self.env.reset()
+        for _ in range(self.k):
+            self.frames.append(ob)
 
-class LazyFrames(object):
-    """A memory efficient buffer for frame tensors."""
+        return self._get_ob()
 
-    def __init__(self, frames):
+    def step(self, action: int) -> tuple:
         """
-        This object ensures that common frames between the observations are
-        only stored once. It exists purely to optimize memory usage which can
-        be huge for DQN's 1M frames replay buffers. This object should only be
-        converted to numpy array before being passed to the model. You'd not
-        believe how complex the previous solution was.
+        Take a step using the given action.
+
+        Args:
+            action: the discrete action to perform.
+
+        Returns:
+            a tuple of:
+            -   the start as a result of the action
+            -   the reward achieved by taking the action
+            -   a flag denoting whether the episode has ended
+            -   a dictionary of extra information
+
         """
-        self._frames = frames
-        self._out = None
+        frame, reward, done, info = self.env.step(action)
+        # add the frame to the queue
+        self.frames.append(frame)
 
-    def _force(self):
-        if self._out is None:
-            self._out = np.concatenate(self._frames, axis=2)
-            self._frames = None
-        return self._out
-
-    def __array__(self, dtype=None):
-        out = self._force()
-        if dtype is not None:
-            out = out.astype(dtype)
-        return out
-
-    def __len__(self):
-        return len(self._force())
-
-    def __getitem__(self, i):
-        return self._force()[i]
+        return self._get_ob(), reward, done, info
 
 
 # explicitly define the outward facing API of this module
