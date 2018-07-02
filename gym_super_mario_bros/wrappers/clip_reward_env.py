872709eefb909e67ed31983ea4c468ab2cdd355e
Kautenja <ckfour@icloud.com>
2018-07-01 20:27:38 -0500

    fix style

--- gym_super_mario_bros/wrappers/clip_reward_env.py
+++ gym_super_mario_bros/wrappers/clip_reward_env.py
@@ -6,11 +6,20 @@ import numpy as np
 class ClipRewardEnv(gym.RewardWrapper):
     """An environment that clips rewards in {-1, 0, 1}."""
 
-    def __init__(self, env):
-        """Initialize a new reward clipping environment."""
-        gym.RewardWrapper.__init__(self, env)
+    def __init__(self, env: gym.Env) -> None:
+        """
+        Initialize a new reward clipping environment.
 
-    def reward(self, reward):
+        Args:
+            env: the environment to wrap
+
+        Returns:
+            None
+
+        """
+        super().__init__(env)
+
+    def reward(self, reward: float) -> float:
         """Bin reward to {-1, 0, +1} using its sign."""
         return np.sign(reward)
 
