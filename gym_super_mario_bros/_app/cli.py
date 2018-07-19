fd913efd095d8631f3ee44eba1e325454fbded15
Kautenja <ckfour@icloud.com>
2018-07-19 12:52:20 -0500

    cleanup

--- gym_super_mario_bros/_app/cli.py
+++ gym_super_mario_bros/_app/cli.py
@@ -1,9 +1,6 @@
 """Super Mario Bros for OpenAI Gym."""
 import os
 import argparse
-from .._registration import make
-from ..wrappers import wrap
-from .play import play_human, play_random
 
 
 def create_argparser() -> argparse.ArgumentParser:
@@ -33,20 +30,20 @@ def create_argparser() -> argparse.ArgumentParser:
 
 def main() -> None:
     """The main entry point for the command line interface."""
-    # parse arguments from the command line (argparse validates arguments)
-    args = create_argparser().parse_args()
-    # select the method for playing the game
-    if args.mode == 'human':
-        os.environ['HUMAN_PLAY'] = str(True)
-        play = play_human
-    elif args.mode == 'random':
-        play = play_random
-    # play the game
-    env = make(args.env)
-    # wrap the environment if specified
-    if args.wrap:
-        env = wrap(env, agent_history_length=None)
-    play(env)
+    # # parse arguments from the command line (argparse validates arguments)
+    # args = create_argparser().parse_args()
+    # # select the method for playing the game
+    # if args.mode == 'human':
+    #     os.environ['HUMAN_PLAY'] = str(True)
+    #     play = play_human
+    # elif args.mode == 'random':
+    #     play = play_random
+    # # play the game
+    # env = make(args.env)
+    # # wrap the environment if specified
+    # if args.wrap:
+    #     env = wrap(env, agent_history_length=None)
+    # play(env)
 
 
 # explicitly define the outward facing API of this module
