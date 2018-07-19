aaa4c5fe5261281ccd4addde23d297a2f43ad07e
Kautenja <ckfour@icloud.com>
2018-07-19 13:00:46 -0500

    disable keys to action

--- gym_super_mario_bros/smb_env.py
+++ gym_super_mario_bros/smb_env.py
@@ -323,33 +323,33 @@ class SuperMarioBrosEnv(NESEnv):
 
 
 
-    def get_keys_to_action(self):
-        """Return the dictionary of keyboard keys to actions."""
-        # Mapping of buttons on the NES joy-pad to keyboard keys
-        up =    ord('w')
-        down =  ord('s')
-        left =  ord('a')
-        right = ord('d')
-        A =     ord('o')
-        B =     ord('p')
-        # a list of keyboard keys with indexes matching the discrete actions
-        # in self.actions
-        keys = [
-            (),
-            (left, ),
-            (right, ),
-            tuple(sorted((left, A, ))),
-            tuple(sorted((left, B, ))),
-            tuple(sorted((left, A, B, ))),
-            tuple(sorted((right, A, ))),
-            tuple(sorted((right, B, ))),
-            tuple(sorted((right, A, B, ))),
-        ]
-        # A mapping of pressed key combinations to discrete actions in action
-        # space
-        keys_to_action = {key: index for index, key in enumerate(keys)}
+    # def get_keys_to_action(self):
+    #     """Return the dictionary of keyboard keys to actions."""
+    #     # Mapping of buttons on the NES joy-pad to keyboard keys
+    #     up =    ord('w')
+    #     down =  ord('s')
+    #     left =  ord('a')
+    #     right = ord('d')
+    #     A =     ord('o')
+    #     B =     ord('p')
+    #     # a list of keyboard keys with indexes matching the discrete actions
+    #     # in self.actions
+    #     keys = [
+    #         (),
+    #         (left, ),
+    #         (right, ),
+    #         tuple(sorted((left, A, ))),
+    #         tuple(sorted((left, B, ))),
+    #         tuple(sorted((left, A, B, ))),
+    #         tuple(sorted((right, A, ))),
+    #         tuple(sorted((right, B, ))),
+    #         tuple(sorted((right, A, B, ))),
+    #     ]
+    #     # A mapping of pressed key combinations to discrete actions in action
+    #     # space
+    #     keys_to_action = {key: index for index, key in enumerate(keys)}
 
-        return keys_to_action
+    #     return keys_to_action
 
 
 # explicitly define the outward facing API of this module
