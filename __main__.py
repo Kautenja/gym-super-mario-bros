b6d2b48690aadf3454b08d97dcad56c115f677fe
Kautenja <ckfour@icloud.com>
2018-07-19 13:00:55 -0500

    simple testing script

--- __main__.py
+++ __main__.py
@@ -1,6 +1,14 @@
 """The main execution script for this package for testing."""
-from gym_super_mario_bros._app.cli import main
+# from gym_super_mario_bros._app.cli import main
 
 
-# execute the main entry point of the CLI
-main()
+# # execute the main entry point of the CLI
+# main()
+
+
+from gym_super_mario_bros import SuperMarioBrosEnv
+from nes_py._app.play_human import play
+
+
+env = SuperMarioBrosEnv()
+play(env, fps=60)
