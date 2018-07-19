13c256298361de7d1ad5244ca4a3edeb9a39a152
Kautenja <ckfour@icloud.com>
2018-07-19 12:50:19 -0500

    fix init

--- gym_super_mario_bros/__init__.py
+++ gym_super_mario_bros/__init__.py
@@ -1,12 +1,8 @@
 """Registration code of Gym environments in this package."""
-from ._registration import make
 from .smb_env import SuperMarioBrosEnv
-from .wrappers import wrap
 
 
 # define the outward facing API of this package
 __all__ = [
-    make.__name__,
     SuperMarioBrosEnv.__name__,
-    wrap.__name__,
 ]
