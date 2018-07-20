919e7ba7da0a65aea9d4555cb9fa168725f00c1d
Kautenja <ckfour@icloud.com>
2018-07-20 12:12:18 -0500

    update main script

--- __main__.py
+++ __main__.py
@@ -1,14 +1,6 @@
 """The main execution script for this package for testing."""
-# from gym_super_mario_bros._app.cli import main
+from gym_super_mario_bros._app.cli import main
 
 
-# # execute the main entry point of the CLI
-# main()
-
-
-from gym_super_mario_bros import SuperMarioBrosEnv
-from nes_py._app.play_human import play
-
-
-env = SuperMarioBrosEnv()
-play(env, fps=60)
+# execute the main entry point of the CLI
+main()
