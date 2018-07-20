aff9309326663d0759c012c4481e411b89566296
Kautenja <ckfour@icloud.com>
2018-07-20 11:24:38 -0500

    update public package API

--- gym_super_mario_bros/__init__.py
+++ gym_super_mario_bros/__init__.py
@@ -1,5 +1,12 @@
 """Registration code of Gym environments in this package."""
+from .smb_env import SuperMarioBrosEnv
+from .smb_level_env import SuperMarioBrosLevelEnv
+from ._registration import make
 
 
 # define the outward facing API of this package
-__all__ = []
+__all__ = [
+    make.__name__,
+    SuperMarioBrosEnv.__name__,
+    SuperMarioBrosLevelEnv.__name__,
+]
