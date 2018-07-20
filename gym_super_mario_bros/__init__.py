b84c07354c5d353ecfb0d60801adc1c59aa0abc1
Kautenja <ckfour@icloud.com>
2018-07-20 01:45:47 -0500

    update __init__

--- gym_super_mario_bros/__init__.py
+++ gym_super_mario_bros/__init__.py
@@ -1,8 +1,5 @@
 """Registration code of Gym environments in this package."""
-from .smb_env import SuperMarioBrosEnv
 
 
 # define the outward facing API of this package
-__all__ = [
-    SuperMarioBrosEnv.__name__,
-]
+__all__ = []
