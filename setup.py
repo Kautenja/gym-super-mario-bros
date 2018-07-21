cc0f59d97126f200420c7c4fe6a4f7de67950a94
Kautenja <ckfour@icloud.com>
2018-07-21 03:08:55 -0500

    bump nes-py version

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     install_requires=[
         'gym>=0.10.5',
         'matplotlib>=2.0.2',
-        'nes-py>=0.7.0',
+        'nes-py>=0.8.4',
         'numpy>=1.14.2',
         'opencv-python>=3.4.0.12',
         'pygame>=1.9.3',
