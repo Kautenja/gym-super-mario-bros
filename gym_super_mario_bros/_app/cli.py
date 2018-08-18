f3d9fec86f212024fa24582d5a492db177ab255b
Kautenja <ckfour@icloud.com>
2018-08-18 00:17:24 -0500

    use _api module from nes_py to reduce redundancy

--- gym_super_mario_bros/_app/cli.py
+++ gym_super_mario_bros/_app/cli.py
@@ -2,14 +2,24 @@
 import os
 import argparse
 from nes_py.wrappers import wrap
+from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv
+from nes_py._app.play import play_human, play_random
 from .._registration import make
-from .play import play_human, play_random
+from ..actions import RIGHT_ONLY, SIMPLE_MOVEMENT, COMPLEX_MOVEMENT
 
 
 # The play modes for the system
 _PLAY_MODES = {
     'human': play_human,
     'random': play_random
+}
+
+
+# a key mapping of action spaces to wrap with
+_ACTION_SPACES = {
+    'right': RIGHT_ONLY,
+    'simple': SIMPLE_MOVEMENT,
+    'complex': COMPLEX_MOVEMENT,
 }
 
 
@@ -20,19 +30,26 @@ def _get_args():
     parser.add_argument('--env', '-e',
         type=str,
         default='SuperMarioBros-v0',
-        help='The name of the environment to play.'
+        help='The name of the environment to play'
     )
     # add the argument for the mode of execution as either human or random
     parser.add_argument('--mode', '-m',
         type=str,
         default='human',
         choices=['human', 'random'],
-        help='The execution mode for the emulation.'
+        help='The execution mode for the emulation'
     )
     # add a flag for wrapping the environment
     parser.add_argument('--wrap', '-W',
         action='store_true',
-        help='A flag to use the standard wrap while playing.'
+        help='A flag to use the standard wrap while playing'
+    )
+    # add the argument for adjusting the action space
+    parser.add_argument('--actionspace', '-a',
+        type=str,
+        default=None,
+        choices=['nes', 'right', 'simple', 'complex'],
+        help='the action space wrapper to use'
     )
     # parse arguments and return them
     return parser.parse_args()
@@ -44,6 +61,12 @@ def main():
     args = _get_args()
     # build the environment with the given ID
     env = make(args.env)
+    # wrap the environment with an action space if specified
+    if args.actionspace != 'nes':
+        # unwrap the actions list by key
+        actions = _ACTION_SPACES[args.actionspace]
+        # wrap the environment with the new action space
+        env = BinarySpaceToDiscreteSpaceEnv(env, actions)
     # wrap the environment if specified
     if args.wrap:
         env = wrap(env, agent_history_length=None)
