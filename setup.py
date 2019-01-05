5483952c99ec9c43da9366667b6efa74325c61ff
Kautenja <kautencreations@gmail.com>
2019-01-05 16:01:29 -0600

    ignore tests in dist

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     author='Christian Kauten',
     author_email='kautencreations@gmail.com',
     license='Proprietary',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests', '*.tests', '*.tests.*']),
     package_data={
         'gym_super_mario_bros': ['_roms/*.nes']
     },
