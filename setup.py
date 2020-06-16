c61fd46d658acc8224bfbd8a874a0d218e7960cf
Christian Kauten <Kautenja@users.noreply.github.com>
2020-06-16 16:15:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Games/Entertainment :: Side-Scrolling/Arcade Games',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
