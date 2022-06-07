53919549d2b49991923cf3a65447e4f8230f3318
Kautenja <ckfour@icloud.com>
2022-06-06 22:35:10 -0600

    bump version to 7.3.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md') as README_md:
 
 setup(
     name='gym_super_mario_bros',
-    version='7.3.2',
+    version='7.3.3',
     description='Super Mario Bros. for OpenAI Gym',
     long_description=README,
     long_description_content_type='text/markdown',
