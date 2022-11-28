89893fb1cf4d75153d228fbc3da288248a4993e1
Soumil32 <soumil009@outlook.com>
2022-11-28 22:45:08 +0000

    removed files from git

---
+++ gym_super_mario_bros/smb_env_straight.py
@@ -0,0 +1,6 @@
+import smb_env
+class SuperMarioBrosEnvStraight(smb_env.SuperMarioBrosEnv):
+    """This environment focuses on just getting Mario form the start of the level to the end of level
+    in the 'traditional' way"""
+
+
