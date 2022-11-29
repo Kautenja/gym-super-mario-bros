b6328a3a5e6ab5d2ec537902aae6c7fdf95afd8e
Soumil32 <soumil009@outlook.com>
2022-11-29 17:39:36 +0000

    wrote the reward methods
    wrote the rest of the reward methods for the straight env

--- gym_super_mario_bros/smb_env_straight.py
+++ gym_super_mario_bros/smb_env_straight.py
@@ -23,12 +23,37 @@ class SuperMarioBrosEnvStraight(smb_env.SuperMarioBrosEnv):
 
 
     @property
-    def x_reward(self):
+    def _x_reward(self):
         """Return the reward for Mario moving in the correct direction"""
         # encourage to move to the right
         x_diff = self.x - self.x_last
         self.x_last = self.x
         # when the game resets, Mario's x position drastically drops
         return x_diff if x_diff > -5 else 0
+
+    @property
+    def _time_reward(self):
+        """Return the time reward for the current step"""
+        time_diff = self.time - self.time_last
+        self.time_last = self.time
+
+        # the time can only increase if the game has been reset
+        if time_diff > 0:
+            return 0
+        # encourage to complete the level as quickly as possible
+        # if the time has decreased, punish the agent otherwise reward it
+        return -1 if time_diff > 0 else 1
+
+    @property
+    def _death_penalty(self):
+        """Return the death penalty for the current step"""
+        # if Mario is dead, punish the agent
+        return -30 if self.is_dead or self._is_dying else 0
+
+    def _get_reward(self):
+        """Return the reward after a step occurs."""
+        return self._x_reward + self._time_reward + self._death_penalty
+
+
 
 
